bcad0f879741f42bee5a85c5f9799899feacc19f
Giulia Ivan <89975187+GiuliaIvan@users.noreply.github.com>
2024-11-10 18:18:19 +0100

    logo to help in visualising the app+AllSet page to complete the onboarding process

---
+++ linder-frontend/src/onboarding/AllSet.jsx
@@ -0,0 +1,11 @@
+function AllSet() {
+  return (
+    <div className="input-field">
+      <h1 className="input-field__title allSet">Congrats!</h1>
+        <h2 className="allSet-h2">You are all set.</h2>
+      {/* <button type="submit">Agree & Join</button> */}
+    </div>
+  )
+}
+
+export default AllSet

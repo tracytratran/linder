66a4d48597b8497573fc6c4cbe0778e06c8b439b
Tracy Tran <tratran.315@gmail.com>
2024-11-25 17:30:55 +0100

    Update styling

--- linder-frontend/src/components/Logo.jsx
+++ linder-frontend/src/components/Logo.jsx
@@ -1,6 +1,9 @@
 function Logo() {
   return (
-    <div className="font-lexend flex justify-center items-center w-full border-b shadow-b mx-auto py-4 bg-sky-700 text-white text-2xl">
+    <div
+      className="flex justify-center items-center w-full border-b shadow-b mx-auto py-4 bg-sky-700 text-white text-lg md:text-2xl"
+      style={{ fontFamily: "'Lexend', serif" }}
+    >
       Linder
     </div>
   );

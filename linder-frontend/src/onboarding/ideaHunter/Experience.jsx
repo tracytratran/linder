3e28d9e12b0372296bdc66edd4768773d6c4aa73
Giulia Ivan <89975187+GiuliaIvan@users.noreply.github.com>
2024-11-27 12:18:05 +0100

    small fixes

--- linder-frontend/src/onboarding/ideaHunter/Experience.jsx
+++ linder-frontend/src/onboarding/ideaHunter/Experience.jsx
@@ -22,17 +22,29 @@ function Experience() {
         </span>
         <label className="w-full">
           Start date*
-          <input type="date" placeholder="dd/mm/yyyy" required className="no-icon" />
+          <input
+            type="date"
+            placeholder="dd/mm/yyyy"
+            required
+            className="no-icon"
+          />
         </label>
         <label className="w-full">
           End date*
-          <input type="date" placeholder="dd/mm/yyyy" required  className="no-icon"/>
+          <input
+            type="date"
+            placeholder="dd/mm/yyyy"
+            required
+            className="no-icon"
+          />
         </label>
         <label className="w-full">
           Location
           <input type="text" placeholder="Aarhus" />
         </label>
-        <button className="w-[30%] mr-auto px-2 py-2.5 bg-blue-600 text-white shadow-md border-0 text-base font-semibold rounded-lg hover:bg-blue-400 transition-all duration-300">+ Add Skill</button>
+        <button onClick={(e)=>e.preventDefault()} className="w-[30%] mr-auto px-2 py-2.5 bg-blue-600 text-white shadow-md border-0 text-base font-semibold rounded-lg hover:bg-blue-400 transition-all duration-300">
+          + Add Skill
+        </button>
       </form>
       {/* <button type="submit">Agree & Join</button> */}
     </div>
